0d949de4e1c3cf7e45c65f4c7393ec15a4297d40
khalid <elfakirkhalid@gmail.com>
2023-05-24 15:56:58 -0700

    function read line

--- functions.c
+++ functions.c
@@ -31,4 +31,119 @@ int func_launch (char **args)
 		while (!WIFEXITED (status) && !WIFSIGNALED (status));
 	}
 	return 1;
+
+	
+/**
+ * func_read_line -  Read a line of input from stdin.
+ * @return The line from stdin
+ *
+ */ 
+char *
+func_read_line (void) 
+{
+  
+#ifdef func_USE_STD_GETLINE
+  char *line = NULL;
+  
+ssize_t bufsize = 0;
+  
+if (getline (&line, &bufsize, stdin) == -1)
+    {
+      
+if (feof (stdin))
+	{
+	  
+exit (EXIT_SUCCESS);
+	
+}
+      else
+	{
+	  
+perror ("func: getline\n");
+	  
+exit (EXIT_FAILURE);
+	
+}
+    
+}
+  
+return line;
+  
+#else	/* 
+ */
+#define func_RL_BUFSIZE 1024
+  int bufsize = func_RL_BUFSIZE;
+  
+int position = 0;
+  
+char *buffer = malloc (sizeof (char) * bufsize);
+  
+int c;
+  
+ 
+if (!buffer)
+    {
+      
+fprintf (stderr, "func: allocation error\n");
+      
+exit (EXIT_FAILURE);
+    
+}
+  
+ 
+while (1)
+    {
+      
+ 
+c = getchar ();
+      
+ 
+if (c == EOF)
+	{
+	  
+exit (EXIT_SUCCESS);
+	
+}
+      else if (c == '\n')
+	{
+	  
+buffer[position] = '\0';
+	  
+return buffer;
+	
+}
+      else
+	{
+	  
+buffer[position] = c;
+	
+}
+      
+position++;
+      
+ 
+ 
+if (position >= bufsize)
+	{
+	  
+bufsize += func_RL_BUFSIZE;
+	  
+buffer = realloc (buffer, bufsize);
+	  
+if (!buffer)
+	    {
+	      
+fprintf (stderr, "func: allocation error\n");
+	      
+exit (EXIT_FAILURE);
+	    
+}
+	
+}
+    
+}
+  
+#endif	/* 
+ */
+}
 }
