9ada48e50d57719cfc2fa5768bc2b9a9314f3c1a
khouleos <khalilbennah@gmail.com>
2023-05-24 16:24:19 -0700

    func_split_line

--- functions.c
+++ functions.c
@@ -143,7 +143,45 @@ exit (EXIT_FAILURE);
     
 }
   
-#endif	/* 
- */
+#endif
 }
+#define func_TOK_BUFSIZE 64
+#define func_TOK_DELIM " \t\r\n\a"
+/**
+ * @brief Split a line into tokens (very naively).
+ * @param line The line.
+ * @return Null-terminated array of tokens.
+ */ 
+char **func_split_line (char *line) 
+{
+	int bufsize = func_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc (bufsize * sizeof (char *));
+	char *token, **tokens_backup;
+
+	if (!tokens)
+	{
+		fprintf (stderr, "func: allocation error\n");
+		exit (EXIT_FAILURE);
+	}
+	token = strtok (line, func_TOK_DELIM);
+	while (token != NULL)
+	{      
+		tokens[position] = token;
+		position++;
+		if (position >= bufsize)
+		{
+			bufsize += func_TOK_BUFSIZE;
+			tokens_backup = tokens;
+			tokens = realloc (tokens, bufsize * sizeof (char *));
+			if (!tokens)
+			{
+				free (tokens_backup);
+				fprintf (stderr, "func: allocation error\n");
+				exit (EXIT_FAILURE);
+			}
+		}
+		token = strtok (NULL, func_TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
